485e743527881de7f18e1e828218d6704104c2c2
Tam Tran <tamtran.js@gmail.com>
2024-06-27 18:08:22 +0700

    + add mongodb client

---
+++ lib/mongodb/client.ts
@@ -0,0 +1,7 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+
+const client = new MongoClient(uri);
+
+export default client;
---
+++ lib/mongodb/movies.ts
@@ -0,0 +1,27 @@
+import client from "./client";
+
+export default async function getMovies(query: string) {
+  try {
+    await client.connect();
+    // set namespace
+    const database = client.db("sample_mflix");
+    const coll = database.collection("movies");
+    // define pipeline
+    const agg = [
+      {
+        $search: {
+          index: "autocomplete-tutorial",
+          autocomplete: { query: query, path: "title" },
+        },
+      },
+      { $limit: 20 },
+      { $project: { _id: 0, title: 1 } },
+    ];
+    // run pipeline
+    const result = await coll.aggregate(agg);
+    // print results
+    await result.forEach((doc) => console.log(doc));
+  } finally {
+    await client.close();
+  }
+}
