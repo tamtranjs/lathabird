ffa6ba67a808502bc7db302cf6505d0c46b73ce5
Trần Minh Tâm <tranminhtam003@gmail.com>
2024-11-08 10:18:23 +0700

    + add background photo in blog detail

--- app/(common)/blogs/[slug]/components/HeadBackground.tsx
+++ app/(common)/blogs/[slug]/components/HeadBackground.tsx
@@ -7,14 +7,16 @@ interface Props {
 export default async function HeadBackground({ blogPost }: Props) {
   const blogPostData = await blogPost;
 
-  const { title, coverImage } = blogPostData;
-  const bgUrl = coverImage.url || "/images/background1.jpg";
+  const { title, coverImage, backgroundImage } = blogPostData;
+  const coverImgUrl = coverImage.url || "/images/background1.jpg";
+
+  const bgrUrl = backgroundImage.url || coverImgUrl;
 
   return (
     <section
       className="relative table w-full items-center py-36 bg-center bg-no-repeat bg-cover"
       style={{
-        backgroundImage: `url(${bgUrl})`,
+        backgroundImage: `url(${bgrUrl})`,
       }}
     >
       <div className="absolute inset-0 bg-gradient-to-b from-slate-900/60 via-slate-900/80 to-slate-900"></div>
