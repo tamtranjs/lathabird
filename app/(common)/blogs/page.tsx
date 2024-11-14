dacb71acc5ec26b265a47f064c17467d00d37384
Trần Minh Tâm <tranminhtam003@gmail.com>
2024-11-14 10:07:10 +0700

    + update layout form search

--- app/(common)/blogs/page.tsx
+++ app/(common)/blogs/page.tsx
@@ -7,20 +7,17 @@ import HeadBackGround from "@/components/layouts/HeadBackGround";
 export const metadata: Metadata = {
   title: "Travel News & Tips | Lathabird",
   description: "Blogs",
-}
+};
 
 export default async function Blogs() {
   return (
     <>
-      <HeadBackGround
-        title="Blogs / News"
-        name="Blogs"
-      />
+      <HeadBackGround title="Blogs / News" name="Blogs" />
       <section className="relative md:py-24 py-16">
         <Suspense fallback={<h2 className="text-center">Loading ...</h2>}>
           <BlogGrid />
         </Suspense>
       </section>
     </>
-  )
+  );
 }
